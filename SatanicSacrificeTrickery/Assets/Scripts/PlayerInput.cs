d12023b0f71e0e6a2d1dabef89aabb824f8407d6
athremist <andrew2255@hotmail.com>
2016-01-29 21:35:33 -0500

    Player movement
    done

---
+++ SatanicSacrificeTrickery/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour
+{
+    const float PLAYER_SPEED = 2;
+
+    Vector2 velocity;
+
+    void Start()
+    {
+        velocity = Vector2.zero;
+    }
+
+    void Update()
+    {
+        Movement();
+        PlayerActions();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, PLAYER_SPEED * Time.deltaTime, 0);
+            //TODO add sprite animation here
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-PLAYER_SPEED * Time.deltaTime, 0, 0);
+            //TODO add sprite animation here
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -PLAYER_SPEED * Time.deltaTime, 0);
+            //TODO add sprite animation here
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(PLAYER_SPEED * Time.deltaTime, 0, 0);
+            //TODO add sprite animation here
+        }
+
+        if (!Input.anyKey)
+        {
+            velocity = Vector2.zero;
+            //TODO add sprite animation here
+        }
+    }
+
+    void PlayerActions()
+    {
+
+    }
+}
