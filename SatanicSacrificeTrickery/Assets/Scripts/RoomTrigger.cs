5da4b9e423d5d7872fdb305151dd39d25005fc21
athremist <andrew2255@hotmail.com>
2016-01-29 22:06:57 -0500

    working on room transitions
    asdkj

---
+++ SatanicSacrificeTrickery/Assets/Scripts/RoomTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomTrigger : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            //Room transition
+        }
+    }
+}
