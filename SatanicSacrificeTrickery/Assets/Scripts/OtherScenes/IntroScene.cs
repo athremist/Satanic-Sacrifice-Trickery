1eddceba2ea85239002fb3577d050ba14f2e3500
athremist <andrew2255@hotmail.com>
2016-01-31 21:42:29 -0500

    Added intro scene
    asd

---
+++ SatanicSacrificeTrickery/Assets/Scripts/OtherScenes/IntroScene.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class IntroScene : MonoBehaviour
+{
+    float m_TextSpeed;
+
+    [SerializeField]
+    string m_Introduction;
+    [SerializeField]
+    Text m_Text;
+    [SerializeField]
+    GameObject m_Skip;
+
+    string m_StoreText;
+
+    void Start()
+    {
+        m_TextSpeed = 0.05f;
+
+        m_StoreText = "";
+
+        StartCoroutine(TextAdder());
+    }
+
+    void Update()
+    {
+        if (m_StoreText.Length == m_Introduction.Length)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Application.LoadLevel(2);
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (m_Skip.activeSelf)
+                {
+                    m_TextSpeed = 0.0001f;
+                    m_Skip.SetActive(false);
+                }
+            }
+            else
+            {
+                if (!m_Skip.activeSelf)
+                {
+                    m_TextSpeed = 0.05f;
+                    m_Skip.SetActive(true);
+                }
+            }
+        }
+    }
+
+    IEnumerator TextAdder()
+    {
+        m_StoreText = "";
+
+        for (int i = 0; i < m_Introduction.Length; i++)
+        {
+            m_StoreText += m_Introduction[i];
+            m_Text.text = m_StoreText;
+
+            yield return new WaitForSeconds(m_TextSpeed);
+        }
+
+        StartCoroutine(GameTransition());
+    }
+
+    IEnumerator GameTransition()
+    {
+        yield return new WaitForSeconds(23.0f);
+        Application.LoadLevel(2);
+    }
+}
